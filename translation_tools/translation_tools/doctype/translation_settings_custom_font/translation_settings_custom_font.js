d9ca35e20a45aaf0a17908c5db4bf942000f17ef
ManotLuijiu <manot_luijiu@hotmail.com>
2025-04-21 17:41:39 +0700

    test on mac

---
+++ translation_tools/translation_tools/doctype/translation_settings_custom_font/translation_settings_custom_font.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Manot Luijiu and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Translation Settings Custom Font", {
+// 	refresh(frm) {
+
+// 	},
+// });
