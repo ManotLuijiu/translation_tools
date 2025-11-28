2568c4db59331861c0cd81b60aef88f0abc98790
Manot Luijiu <manot.luijiu@gmail.com>
2025-11-28 18:12:26 +0700

    Fixed workspace

---
+++ translation_tools/translation_tools/doctype/workspace_config_card/workspace_config_card.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Manot Luijiu and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class WorkspaceConfigCard(Document):
+	pass
